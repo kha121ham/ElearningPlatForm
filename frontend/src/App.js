5586c2609fc803ff490cfd00620099900e829875
Khaled Hamada <khaledhamada779@gmail.com>
2025-03-07 17:58:23 +0200

    Tailwind setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
   return (
-    <div>
-      welcome
+    <div className="min-h-screen flex flex-col">
+    {/* Header */}
+  <header className="bg-blue-600 text-white py-4">
+    <div className="container mx-auto px-4">
+      <h1 className="text-2xl font-bold">My Website</h1>
     </div>
+  </header>
+  </div>
   )
 }
 
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+export const content = [
+  "./src/**/*.{js,jsx,ts,tsx}", // تحليل جميع ملفات JS/JSX/TS/TSX في مجلد src
+  "./public/index.html", // تحليل ملف HTML الرئيسي
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
